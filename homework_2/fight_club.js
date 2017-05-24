d7363db2e4b9056d9267192db0ebfe858e31693f
chulanovskyi <skyrocker@mail.ru>
2017-05-24 14:23:29 +0300

    homework ready

---
+++ homework_2/fight_club.js
@@ -0,0 +1,65 @@
+'use strict'
+class Fighter{
+    constructor(name, health, power){
+        this._name = name;
+        this._health = health;
+        this._power = power;
+    }
+
+    setDamage(damage){
+        this._health -= damage;
+        if (this._health < 0){
+            this._health = 0;
+        }
+        console.log(`${this._name} health: ${this._health}`);
+    }
+
+    hit(enemy, point = 1){
+        let damage = this._power * point;
+        enemy.setDamage(damage);
+    }
+
+    toString(){
+        return `Fighter ${this._name} (${this._health})`
+    }
+}
+
+class ImporovedFighter extends Fighter{
+    doubleHit(enemy, point = 1){
+        super.hit(enemy, point * 2);
+    }
+
+    toString(){
+        return `Imporoved fighter ${this._name} (${this._health})`
+    }
+}
+
+function fight(fighter, iFighter, ...point){
+    console.log('Fight!');
+    
+    var randomPoint = () => point[Math.floor(Math.random() * point.length)];
+    var doubleDamage = () => Math.round(Math.random());
+
+    while (fighter._health > 0 && iFighter._health > 0){
+        fighter.hit(iFighter, randomPoint());
+        if (iFighter._health > 0){
+            if (doubleDamage()){
+                console.log('Critical hit!');
+                iFighter.doubleHit(fighter, randomPoint());
+            } else {
+                iFighter.hit(fighter, randomPoint());
+            }
+        }
+    }
+
+    let winner = fighter._health > 0 ? fighter : iFighter;
+    console.log(`And the winner is ${winner._name}!`);
+}
+
+let madMax = new Fighter('Mad Max', 100, 1)
+let subZero = new ImporovedFighter('Sub-Zero', 100, 1)
+let point = [15, 19, 25, 10, 77, 30, 5, 7];
+
+fight(madMax, subZero, ...point);
+
+console.log(`Results:\n${madMax.toString()}\n${subZero.toString()}`);
