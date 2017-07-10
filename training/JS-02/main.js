5d3e306607dbb9f47b6d41fa1dba9607ac2775fd
chulanovskyi <skyrocker2013@gmail.com>
2017-07-10 23:44:58 +0300

    header hider in progress

--- training/JS-02/main.js
+++ training/JS-02/main.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const VISIBLE = 'visible';
+let headerNav = document.getElementsByClassName('section__nav')[0];
+let headerSearch = document.getElementsByClassName('section__search')[0];
+let headerHider = document.getElementsByClassName('header__hider')[0];
+function toggleHeader() {
+    if(headerNav.classList.contains(VISIBLE)){
+        headerNav.style.display = 'none';
+        headerNav.classList.remove('visible');
+    } else {
+        headerNav.style.display = 'block';
+        headerNav.classList.add('visible');
+    }
+}
