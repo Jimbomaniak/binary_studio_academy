ab8db2136689a3321c2884e6112387f12e92f4b0
chulanovskyi <skyrocker2013@gmail.com>
2017-07-19 13:46:01 +0300

    minor fix

--- training/JS-06/messapp-xhr.js
+++ training/JS-06/messapp-xhr.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index-xhr.html'))
 });
 
 app.get('/services/chat_ajax.js', (req, res) => {
