0c39d769460e4885f79a05a54f4302ec410240de
chulanovskyi <skyrocker2013@gmail.com>
2017-07-23 01:45:02 +0300

    task ready

--- training/JS-07/src/ts/fightClub.js
+++ training/JS-07/src/ts/fightClub.js
@@ -1,55 +1,45 @@
 "use strict";
-class Fighter {
-    constructor(name, health, power) {
-        this.name = name;
-        this.health = health;
-        this.power = power;
-    }
-    setDamage(damage) {
-        this.health -= damage;
-        if (this.health < 0) {
-            this.health = 0;
-        }
-        console.log(`${this.name} health: ${this.health}`);
-    }
-    hit(enemy, point = 1) {
-        let damage = this.power * point;
-        enemy.setDamage(damage);
-    }
-    toString() {
-        return `Fighter ${this.name} (${this.health})`;
-    }
-}
-class ImprovedFighter extends Fighter {
-    doubleHit(enemy, point = 1) {
-        super.hit(enemy, point * 2);
-    }
-    toString() {
-        return `Improved fighter ${this.name} (${this.health})`;
-    }
-}
-function fight(fighter, improvedFighter, ...point) {
-    console.log('Fight!');
-    let randomPoint = () => point[Math.floor(Math.random() * point.length)];
-    let doubleDamage = () => Math.round(Math.random());
-    while (fighter.health > 0 && improvedFighter.health > 0) {
-        fighter.hit(improvedFighter, randomPoint());
-        if (improvedFighter.health > 0) {
-            if (doubleDamage()) {
-                console.log('Critical hit!');
-                improvedFighter.doubleHit(fighter, randomPoint());
-            }
-            else {
-                improvedFighter.hit(fighter, randomPoint());
+Object.defineProperty(exports, "__esModule", { value: true });
+const fighter_1 = require("./fighter");
+const improvedFighter_1 = require("./improvedFighter");
+class Fight {
+    static fight(fighter, improvedFighter, ...point) {
+        let page = document.getElementsByClassName('container__fight')[0];
+        Fight._message(`${fighter.toString()} vs ${improvedFighter.toString()}`, page);
+        Fight._message(`Let the fight begin!`, page);
+        while (fighter.health > 0 && improvedFighter.health > 0) {
+            let hitF = fighter.hit(improvedFighter, Fight._getRandomPoint(point));
+            Fight._message(hitF, page);
+            if (improvedFighter.health > 0) {
+                if (Fight._isDouble()) {
+                    let hitDD = improvedFighter.doubleHit(fighter, Fight._getRandomPoint(point));
+                    Fight._message(`Critical hit! ${hitDD}`, page);
+                }
+                else {
+                    let hitI = improvedFighter.hit(fighter, Fight._getRandomPoint(point));
+                    Fight._message(`${hitI}`, page);
+                }
             }
         }
+        let winner = fighter.health > 0 ? fighter : improvedFighter;
+        Fight._message(`And the winner is ${winner.name}!`, page);
+        Fight._message(`${fighter.toString()} | ${improvedFighter.toString()}`, page);
     }
-    let winner = fighter.health > 0 ? fighter : improvedFighter;
-    console.log(`And the winner is ${winner.name}!`);
+    static _isDouble() {
+        return Math.round(Math.random());
+    }
+    static _getRandomPoint(points) {
+        return points[Math.floor(Math.random() * points.length)];
+    }
+    static _message(text, el) {
+        let message = document.createElement('div');
+        message.classList.add('fight__message');
+        message.innerHTML = text;
+        el.appendChild(message);
+    }
 }
-let madMax = new Fighter('Mad Max', 100, 1);
-let subZero = new ImprovedFighter('Sub-Zero', 100, 1);
+let madMax = new fighter_1.default('Mad Max', 100, 1);
+let subZero = new improvedFighter_1.default('Sub-Zero', 100, 1);
 let point = [15, 19, 25, 10, 77, 30, 5, 7];
-fight(madMax, subZero, ...point);
-console.log(`Results:\n${madMax.toString()}\n${subZero.toString()}`);
+Fight.fight(madMax, subZero, ...point);
 //# sourceMappingURL=fightClub.js.map
